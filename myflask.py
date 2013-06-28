b431db0624cf457ad86f2fdf9a3fc341f5913ef7
Ubuntu <ubuntu@ip-172-31-29-162.us-west-2.compute.internal>
2013-06-28 00:00:30 +0000

    added show me more

--- myflask.py
+++ myflask.py
@@ -19,15 +19,44 @@ def home():
 def home():
         return render_template('about.html')
 
-@app.route('/test')
-def home():
-        return render_template('d3test.html')
 
 #display form to get user input (city name)
 @app.route('/',methods = ['GET','POST'])
 @app.route('/start',methods = ['GET','POST'])
 def start():
         return render_template('start.html')
+
+# how more results 
+@app.route('/showmore', methods = ['POST'])
+def showmore():
+    rec =[]
+    homecity = request.form['homecity']
+    visitcity = request.form['visitcity']
+    if not homecity:
+        homecity = 'atlanta'
+    if not visitcity:
+        visitcity = 'new york'
+    db = MySQLdb.connect(host = 'localhost',user = 'root',passwd = 'insight', db = 'bestfood_AWS')
+    cursor = db.cursor()
+    cursor.execute("USE bestfood_AWS")
+    sql = "select * from acad_topitems where homecity=\'"+visitcity+"\' AND visitcity= \'"+homecity+"\'  order by rank asc limit 5"
+    df_mysql = psql.frame_query(sql, con=db)
+    foodlist =[]
+    for i in range(2,len(df_mysql)):
+        food = df_mysql['term'][i]
+        rname = df_mysql['rest'][i]
+        url = df_mysql['url'][i]
+        ratio = df_mysql['popularity_ratio'][i]
+	rec = [food,rname,url,ratio]
+        foodlist.append(rec)
+    wordlist=[]
+    wordlist.append(rec)
+    wordlist.append(visitcity)
+    #random.shuffle(foodlist)
+    return render_template('results_more.html', visitcity = visitcity, wordlist = foodlist) 
+    #return render_template('results.html', wordlist = wordlist) 
+
+
 
 # get user input from templates/start.html and pass on to analysis script
 @app.route('/find', methods = ['POST'])
@@ -39,40 +68,24 @@ def find():
         homecity = 'atlanta'
     if not visitcity:
         visitcity = 'new york'
-    db = MySQLdb.connect(host = 'localhost',user = 'root',passwd = '', db = 'bestfood')
+    db = MySQLdb.connect(host = 'localhost',user = 'root',passwd = 'insight', db = 'bestfood_AWS')
     cursor = db.cursor()
-    cursor.execute("USE bestfood")
-    sql = "select * from Acad_topitems where homecity=\'"+visitcity+"\' AND visitcity= \'"+homecity+"\'  order by rank asc limit 4"
+    cursor.execute("USE bestfood_AWS")
+    sql = "select * from acad_topitems where homecity=\'"+visitcity+"\' AND visitcity= \'"+homecity+"\'  order by rank asc limit 3"
     df_mysql = psql.frame_query(sql, con=db)
     foodlist =[]
-    for i in range(0,len(df_mysql)):
+    for i in range(0,2):
         food = df_mysql['term'][i]
         rname = df_mysql['rest'][i]
         url = df_mysql['url'][i]
         ratio = df_mysql['popularity_ratio'][i]
-   	# figure out which restaurant for the food item
-  	#sql = "select bid from Acad_reviews where city = \'"+visitcity+"\' AND text like \'%"+food+"%\' order by stars desc limit 1"
-	#df_bid = psql.frame_query(sql, con=db)
-	#bid = df_bid['bid'][0]
-	#sql = "select name,url from acad_restaurants where bid =\'"+bid+"\'"
-	#df_rest = psql.frame_query(sql, con=db)
-	
-	"""
-	topRests = find_restaurant_for_food(food,visitcity,db)
-	rname = topRests[0][0]
-	url = topRests[0][1]
-	"""
-	#rname = df_rest['name'][0]
-	#url = df_rest['url'][0]
-	#rname = 'dummy'
-	#url = 'dummy url'
 	rec = [food,rname,url,ratio]
         foodlist.append(rec)
     wordlist=[]
     wordlist.append(rec)
     wordlist.append(visitcity)
     #random.shuffle(foodlist)
-    return render_template('results.html', visitcity = visitcity, wordlist = foodlist) 
+    return render_template('results.html', homecity = homecity, visitcity = visitcity, wordlist = foodlist) 
     #return render_template('results.html', wordlist = wordlist) 
 
 
@@ -98,5 +111,5 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     #port = int(os.environ.get('PORT', 5000))
     app.debug = True
-    #app.run(host='0.0.0.0', port=port)
-    app.run()
+    app.run(host='0.0.0.0', port=80)
+    #app.run()
